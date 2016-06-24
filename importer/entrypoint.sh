97c5d718b5e8cf2f30a1311db1b2f8874af80c76
Ludovic Vannoorenberghe <ludo@datacamp.com>
2016-06-24 12:57:56 +0200

    takes config from env

--- importer/entrypoint.sh
+++ importer/entrypoint.sh
@@ -2,6 +2,7 @@
 set -e
 
 dockerize \
+  -template /opt/elasticsearch-jdbc/data/"$@":/tmpl/"$@" \
   -stdout /opt/elasticsearch-jdbc/logs/jdbc.log \
   -stdout /opt/elasticsearch-jdbc/data/statefile.json \
   java \
@@ -9,4 +10,4 @@ dockerize \
   -Dlog4j.configurationFile=/opt/elasticsearch-jdbc/bin/log4j2.xml \
   org.xbib.tools.Runner \
   org.xbib.tools.JDBCImporter \
-  /opt/elasticsearch-jdbc/data/"$@"
+  /tmpl/"$@"
