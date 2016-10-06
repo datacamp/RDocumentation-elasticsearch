ad1c91dbbc6e78fed6037329f9f3f0d98620833c
Ludovic Vannoorenberghe <ludo@datacamp.com>
2016-10-06 20:52:13 +0200

    log only to stdout

--- importer/entrypoint.sh
+++ importer/entrypoint.sh
@@ -3,11 +3,9 @@ set -e
 
 dockerize \
   -template /opt/elasticsearch-jdbc/data/"$@":/tmpl/"$@" \
-  -stdout /opt/elasticsearch-jdbc/logs/jdbc.log \
-  -stdout /opt/elasticsearch-jdbc/data/statefile.json \
   java \
   -cp "/opt/elasticsearch-jdbc/lib/*" \
-  -Dlog4j.configurationFile=/opt/elasticsearch-jdbc/bin/log4j2.xml \
+  -Dlog4j.configurationFile=/opt/elasticsearch-jdbc/bin/logging.xml \
   org.xbib.tools.Runner \
   org.xbib.tools.JDBCImporter \
   /tmpl/"$@"
